4ab6b18d345fa3a16024e7acbc3799576ba956cb
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2016-11-11 16:03:22 -0600

    Basic index.php template

---
+++ Website_Files/home.php
@@ -0,0 +1,10 @@
+<?php include_once("template-parts/header.php"); ?>
+<main>
+<div class="container-fluid">
+
+</div>
+</main>
+
+<?php include_once("template-parts/footer.php"); ?>
+
+<!-- general template -->
--- Website_Files/index.php
+++ Website_Files/index.php
@@ -1,6 +1,43 @@
 <?php include_once("template-parts/header.php"); ?>
 <main>
-<div class="container-fluid">
+<div class="container-fluid"> 
+	
+	<!-- start 'Introducing RC' -->
+	<section>
+
+		<!-- background image -->
+		<h1>Introducing</h1>	
+		<img src="images/logo" alt="RoundCare">
+		<a href="#" class="rc-btn-big">Learn More!</a>
+	
+	</section> <!-- end 'Introducing RC' -->
+
+	<!-- start 'available at' -->
+	<section>
+		<h3>Available at these locations April 15, 2017.</h3>
+
+		<a href="#" class="rc-btn">Company 1</a>
+		<a href="#" class="rc-btn">Company 2</a>
+		<a href="#" class="rc-btn">Company 3</a>
+		<a href="#" class="rc-btn">Company 4</a>
+	</section>
+	<!-- end 'available at' -->
+
+	<!-- start 'video testimonials' -->
+	<section>
+	<h2>Video Testimonials</h2>
+	<!-- video carousel goes here - more research needed-->
+	</section> 
+	<!-- end 'video testimonials' -->
+
+	<!-- start 'reviews' -->
+	<section>
+		<h2>Product Reviews</h2>
+		<!-- photo gallery goes here -->
+	</section>
+	<!-- end 'reviews here' -->
+
+
 
 </div>
 </main>
