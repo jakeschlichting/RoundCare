180cfbd3ecb66186c49f7b7a57ce00edc4f484e3
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2016-11-12 10:18:19 -0600

    create getstarted.php basic template

---
+++ Website_Files/getstarted.php
@@ -0,0 +1,49 @@
+<?php include_once("template-parts/header.php"); ?>
+<main>
+<div class="container-fluid">
+
+	<!-- start page title -->
+	<section>
+		<!-- background image -->
+		<h1>Get Started</h1>
+	</section> <!-- end 'page title -->
+
+	<!-- start  -->
+	<section>
+		<!-- mobile pics stack. desktop image gallery. -->
+		<div>
+			<img src="images/" alt="">
+			<img src="images/" alt="">
+			<img src="images" alt="">
+		</div>
+		<p>text</p>
+	</section>	
+	<!-- end -->
+
+	<!-- start 'what rc can do' -->
+	<section>
+		<h2>What RoundCare Can Do</h2>
+		<!-- insert tile gallery here icons/imgs and text to swap out -->
+	</section>
+	
+	<!-- start infographic -->
+	<section>
+		<img src="images/" alt="">
+	</section>
+	<!-- end infographic -->
+
+	<!-- start questions -->
+	<section>
+		<h2>Questions?</h2>
+		<a href="support.php">Check out our Support Page</a>
+	</section>
+
+
+
+
+</div> <!-- end .container-fluid -->
+</main>
+
+<?php include_once("template-parts/footer.php"); ?>
+
+<!-- general template -->
