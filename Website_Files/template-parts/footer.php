882a0088b6bb9dd49b49f590f504de951a641216
alexwcarlson <awcarlson@gmail.com>
2016-11-12 11:47:34 -0600

    footer links added, footer styling

--- Website_Files/template-parts/footer.php
+++ Website_Files/template-parts/footer.php
@@ -1,5 +1,38 @@
 <footer>
-
+    <div class="container-fluid">
+<div class="row">
+    <div class="col-sm-3 footer-nav">
+        <ul>
+            <li><a href="/index.html">Home</a></li>
+            <li><a href="/aboutus">About US</a></li>
+            <li><a href="/product">Product Info</a></li>
+            <li><a href="/getstarted"> Get Started</a></li>
+        </ul>
+    </div>
+    <div class="col-sm-3 footer-nav">
+        <ul>
+            <li><a href="#">Support</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Referral</a></li>
+            <li><a href="#">Career</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+    <div class="col-sm-3 footer-nav">
+        <ul>
+        <li><a href="#">Contact</a></li>
+        </ul>
+    </div>
+    <div class="col-sm-3 footer-nav">
+        <h3>Follow Us</h3>
+        <ul class="social">
+            <li><a href="http://www.facebook.comd"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+            <li><a href="http://www.instagram.com"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            <li><a href="http://www.pinterest.com"><i class="fa fa-pinterest-square" aria-hidden="true"></i></a></li>
+        </ul>
+    </div>
+</div> <!--end footer row -->
+    </div><!--end footer container -->
 </footer>
 
 
