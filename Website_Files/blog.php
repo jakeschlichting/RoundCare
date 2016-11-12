dbf8a5745d6679fc375683f2d516e6ec7b084740
alexwcarlson <awcarlson@gmail.com>
2016-11-12 11:50:48 -0600

    footer changes

---
+++ Website_Files/blog.php
@@ -0,0 +1,62 @@
+<?php include_once("template-parts/header.php"); ?>
+<main>
+<div class="container-fluid">
+
+	<!-- start page title -->
+	<section>
+		<!-- background image -->
+		<h1>Blog</h1>
+	</section> <!-- end 'page title -->
+
+	<!-- start blog articles section-->
+	<section>
+		<div> <!-- start list of articles -->
+			<article>
+				<img src="" alt="">
+				<p> <strong>Date</strong></p>
+				<h4>Article Title</h4>
+				<p>article text</p>
+				<a href="" class="rc-btn">Read More</a>
+			</article>
+
+			<article>
+				<img src="" alt="">
+				<p> <strong>Date</strong></p>
+				<h4>Article Title</h4>
+				<p>article text</p>
+				<a href="" class="rc-btn">Read More</a>
+			</article>
+
+			<article>
+				<img src="" alt="">
+				<p> <strong>Date</strong></p>
+				<h4>Article Title</h4>
+				<p>article text</p>support
+				<a href="" class="rc-btn">Read More</a>
+			</article>
+			
+		</div> <!-- end list of articles -->
+
+
+		<aside>
+			<div> background image on desktop. hidden on mobile.</div>
+			<div>
+				list of categories -links? sort? 
+			</div>  <!-- end categories listing -->
+
+			<div>search articles
+			</div>
+		</aside>
+	</section>	<!-- end blog articles-->
+
+
+
+	
+
+
+</div> <!-- end .container-fluid -->
+</main>
+
+<?php include_once("template-parts/footer.php"); ?>
+
+<!-- general template -->
