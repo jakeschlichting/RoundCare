f721206e1b7fecbaa7362b6c032ff7000129b0c1
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2016-11-12 10:38:24 -0600

    create aboutus.php basic template

---
+++ Website_Files/aboutus.php
@@ -0,0 +1,36 @@
+<?php include_once("template-parts/header.php"); ?>
+<main>
+<div class="container-fluid">
+
+	<!-- start page title -->
+	<section>
+		<!-- background image -->
+		<h1>About Us</h1>
+	</section> <!-- end 'page title -->
+
+	<!-- start  mission-->
+	<section>
+		<p>text</p>
+		<img src="images/" alt="">
+	</section>	<!-- end mission-->
+
+	<!-- start staff pics -->
+	<section>
+		<h2>About Us</h2>
+		<img src="" alt="">
+		<img src="" alt="">
+		<img src="" alt="">
+		<img src="" alt="">
+		<img src="" alt="">
+		<img src="" alt="">
+
+	</section>	<!-- end staff pics -->
+
+
+
+</div> <!-- end .container-fluid -->
+</main>
+
+<?php include_once("template-parts/footer.php"); ?>
+
+<!-- general template -->
