1e72887df802adf18d7f853cc93fb26de1b340e1
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2016-11-12 14:40:23 -0600

    create career.php basic template

---
+++ Website_Files/career.php
@@ -0,0 +1,29 @@
+<?php include_once("template-parts/header.php"); ?>
+<main>
+<div class="container-fluid"> 
+	
+	<!-- start 'title' -->
+	<section class="row hero-image">
+		<!-- background image -->
+		<h1>Join Us</h1>
+	</section> <!-- end title -->
+
+
+	<!-- start 'why work here' -->
+	<section class="row">
+	<h2>Why RoundCare?</h2>
+	<p>text</p>
+	<img src="" alt="" class="img-responsive">
+	<img src="" alt="" class="img-responsive">
+	<img src="" alt="" class="img-responsive">
+	</section> 
+	<!-- end 'why work here' -->
+
+
+
+</div>
+<!-- end .container-fluid -->
+</main>
+
+<?php include_once("template-parts/footer.php"); ?>
+
