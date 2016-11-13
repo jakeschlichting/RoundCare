673759f6af847005bde1ee244e7b33e74bbef6d5
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2016-11-13 05:13:19 -0600

    reconfig code kit compilation path. add text to product.php

--- Website_Files/product.php
+++ Website_Files/product.php
@@ -5,14 +5,33 @@
 	<!-- start page title -->
 	<section class="row hero-image" style="background-image: url('images/iphone-puppy-productPage.jpg');">
 		<!-- background image -->
-
+		<h1>RoundCare</h1>
 	</section> <!-- end 'page title -->
 
 	<!-- start  the product-->
+	<section class="row background-blue">
+		<div class="col-sm-12">
+			<h2>RoundCare is 21st Century Petcare Care</h2>
+			<p>The RoundCare pet monitor is a must for your 21st Century family. Whether in the other room, driving in your car running errands, or at work, with RoundCare pet monitor you can keep track of all of your pets activities 24/7. Don’t want to spend a fortune on pet daycare or a pet nanny? Fear separation anxiety? RoundCare is an affordable and smart alternative. </p>
+		</div>
+	</section>	<!-- end product info-->
+
+	<!-- start  the product-->
 	<section class="row">
-		<h2>The Product</h2>
-		<img src="images/" alt="" class="img-responsive">
-		<p>text</p>
+		<div class="col-sm-12">
+			<h2>Our interactive pet camera has many awesome features.</h2>
+			<div class="icon" id="wireless"></div>
+			<div class="icon" id="waterproof"></div>
+			<div class="icon" id="zoom"></div>
+			<div class="icon" id="music"></div>
+			<div class="icon" id="battery"></div>
+
+			<p>Roundcare will arrive ready to be connected to all of your wireless and bluetooth devices. Connect the pet monitor to your smartphone, television, car, tablet, and computer.</p>
+			<p>Not only is RoundCare lightweight and able to fit into a variety of places throughout your home, but it is also waterproof. You never have to worry about your device getting wet.</p>
+			<p>Equipped with a powerful zoom, RoundCare camera lens is state-of-the-art. It can zoom over 700 feet and share crystal clear images of your pet while you are away.</p>
+			<p>RoundCare is equipped with a radio function. Play a variety of music styles to sooth or entertain your pet while you are away. This is especially a great option for bird owners.</p>
+			<p>There is no need to worry about placement near an outlet because RoundCare runs on two rechargable AA batteries.</p>
+		</div>
 	</section>	<!-- end product info-->
 
 	
